97fe7b6e4aa56e9531ce92a4ded9683ea952b44a
Patrick Laughlin <patricklaughlin@gmail.com>
2013-07-17 14:55:09 -0500

    config syncs

--- htdocs/wp-local-config.php
+++ htdocs/wp-local-config.php
@@ -16,22 +16,26 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'gittest_kc');
+define('DB_NAME', 'gittest_db');
 
 /** MySQL database username */
-define('DB_USER', 'gittest');
+define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', '8YX8oWZgyZdG');
+define('DB_PASSWORD', 'root');
 
 /** MySQL hostname */
-define('DB_HOST', 'mysql.work.kirkpatrickcreative.com');
+define('DB_HOST', 'localhost');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
 
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
+
+// Overwrites the database to save keep edeting the DB
+define('WP_HOME','http://gittest.dev');
+define('WP_SITEURL','http://gittest.dev');
 
 /**#@+
  * Authentication Unique Keys and Salts.
@@ -42,14 +46,14 @@ define('DB_COLLATE', '');
  *
  * @since 2.6.0
  */
-define('AUTH_KEY',         'Wz|^$Jhv#;6K7A^}(voB?XO=5|*0-@ry.}[oI-`bH3AshHah35,58K2shE$&p!rH');
-define('SECURE_AUTH_KEY',  'Y+nQK&c Q^IFY{g]bC}k[FnogEjKb-fTBlLdyd}Lz.2vgwv&,TbC%g:3~y:|[`-@');
-define('LOGGED_IN_KEY',    'c3J4+(5vhCqc=r[MBY~|r73c4<!|+(ZQkO_62B+vhYt]dT0y71CC+@0 %Anud+ch');
-define('NONCE_KEY',        'y{m**Wi(R$~3`$}-k*d5L!(]4JuaAs=-wP,d:ve%ncA]TjVVp,K5EH+v]/f5v3,~');
-define('AUTH_SALT',        'Hyo=N;M6W?wFkk=W&}ib~/-IFUW7H#bIEbJld/+**Y9.[mAC)b7D9GH#,-t8DMz2');
-define('SECURE_AUTH_SALT', '8rRIA ]]izC=<+}+ 3=J0DVZ{^ioMV/z+mo~Mc1W[+jQwQ@+sr:Z*p{km.dd:p<(');
-define('LOGGED_IN_SALT',   '#W8GEy4!7jzDV[)w5jVN+h(n}{A|iD;++y(Ng;,s5e2q?ZyYxXr7=ZqnKq2oN+CB');
-define('NONCE_SALT',       'xq|>M->*/u7kF 20Z#j^*<+XGh;[G`C>DaGYL:7xZ8xVWB^+l/x8-B5/x3./;;VM');
+define('AUTH_KEY',         '/w-tOEUR6;<:Xp *p5`EtgUu8/Sw+7^svkJ:*L]]5Ximcq,$s3P^Er|9+.hiF^4i');
+define('SECURE_AUTH_KEY',  '&--TYRM>08vBo;HwNMKYzBu9MLzjM[w~/l*#,K+LR)acfq:Pw>Mp#IVI5]rR~8;h');
+define('LOGGED_IN_KEY',    'xSzg-cQpS9cQt+b^PgVe$/$;aR2NiL9C7[.o3b^p{Q@&PZe)0/QYQRc1t4n*&+?[');
+define('NONCE_KEY',        'H}>p]d^z-VM%19/GM!]-[]C|nIb@~c!/ ouzn?^3R{-NAE@h&uL]4o`?3whtR|aP');
+define('AUTH_SALT',        'AH`&F,bAjms);IU<_F%?9<p|8,ZM?b#!B)MN3j^w&g]9qM[EW45RM#FejS61Z+eR');
+define('SECURE_AUTH_SALT', '^Md+9]g:]*i9Hlk1h-GI34hf |1-nO?!!^6hnl,0q]w{A!|g{b|/!mLolZr[+v(.');
+define('LOGGED_IN_SALT',   'r{[.b9KLNZ8p^ihR>476,P.C,h7>G:?PC)//ulu>f$5wb+a?aZS}zB8@)^k:I!C)');
+define('NONCE_SALT',       '%P+L+HW-9XDF66My,c?i@pU+|fnH=SuIu*S8b<W&shX0vurFn+)NkdC|.?)JAg /');
 
 /**#@-*/
 
@@ -78,7 +82,7 @@ define('WPLANG', '');
  * It is strongly recommended that plugin and theme developers use WP_DEBUG
  * in their development environments.
  */
-define('WP_DEBUG', false);
+define('WP_DEBUG', true);
 
 /* That's all, stop editing! Happy blogging. */
 
